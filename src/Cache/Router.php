358dec6cc1725d9057155661d6f1cc5802aee7d2
Phil Bennett <mail@philbennett.co.uk>
2024-11-09 16:16:42 +0000

    Updated caching router to accept a custom cache key

--- src/Cache/Router.php
+++ src/Cache/Router.php
@@ -3,7 +3,7 @@
 /**
  * The cached router is currently in BETA and not recommended for production code.
  *
- * Please feel free to heavily test and report any issues as an issue on the Github repository.
+ * Please feel free to heavily test and report any issues as an issue on the GitHub repository.
  */
 
 declare(strict_types=1);
@@ -19,8 +19,6 @@ use function Opis\Closure\{serialize as s, unserialize as u};
 
 class Router
 {
-    protected const CACHE_KEY = 'league/route/cache';
-
     /**
      * @var callable
      */
@@ -34,7 +32,8 @@ class Router
     public function __construct(
         callable $builder,
         protected CacheInterface $cache,
-        protected bool $cacheEnabled = true
+        protected bool $cacheEnabled = true,
+        protected string $cacheKey = 'league/route/cache'
     ) {
         $this->builder = $builder;
     }
@@ -47,7 +46,7 @@ class Router
 
     protected function buildRouter(ServerRequestInterface $request): MainRouter
     {
-        if (true === $this->cacheEnabled && $cache = $this->cache->get(static::CACHE_KEY)) {
+        if (true === $this->cacheEnabled && $cache = $this->cache->get($this->cacheKey)) {
             $router = u($cache, ['allowed_classes' => true]);
 
             if ($router instanceof MainRouter) {
@@ -64,7 +63,7 @@ class Router
 
         if ($router instanceof MainRouter) {
             $router->prepareRoutes($request);
-            $this->cache->set(static::CACHE_KEY, s($router));
+            $this->cache->set($this->cacheKey, s($router));
             return $router;
         }
 
